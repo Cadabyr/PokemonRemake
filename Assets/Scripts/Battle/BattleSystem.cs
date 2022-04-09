4bcae0c6f527cc3b7b4fda0bec75529e7c78c94c
Chris Stump <ChrisMichaelStump@gmail.com>
2022-04-09 13:43:03 -0400

    Added battle dialog

--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -6,9 +6,9 @@ public class BattleSystem : MonoBehaviour
 {
     [SerializeField] BattleUnit playerUnit;
     [SerializeField] BattleHUD playerHud;
-
     [SerializeField] BattleUnit enemyUnit;
     [SerializeField] BattleHUD enemyHud;
+    [SerializeField] BattleDialogBox dialogBox;
 
     private void Start(){
         SetupBattle();
@@ -19,5 +19,7 @@ public class BattleSystem : MonoBehaviour
         enemyUnit.Setup();
         playerHud.SetData(playerUnit.Pokemon);
         enemyHud.SetData(enemyUnit.Pokemon);
+
+        StartCoroutine(dialogBox.TypeDialog($"A wild {playerUnit.Pokemon.Base.Name} appeared."));
     }
 }
